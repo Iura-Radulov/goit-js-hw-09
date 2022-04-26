a7e360f939469e43a24ca6efcaa3e7abd6ebdda0
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-04-26 14:30:13 +0300

    Exercise 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtnEl = document.querySelector('button[data-action="start"]');
+const stopBtnEl = document.querySelector('button[data-action="stop"]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+startBtnEl.addEventListener("click", onStartBtnClick);
+stopBtnEl.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick() {
+    startBtnEl.disabled = true;
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        bodyEl.style.backgroundColor = color;        
+    }, 1000)
+}
+function onStopBtnClick() {
+    clearInterval(timerId);
+    startBtnEl.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
